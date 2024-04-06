cf090d0732ff8f84885b4a2ab57d775f9520734a
williford-abunas <abunaswilliford@gmail.com>
2024-04-07 02:22:25 +1200

    adjust logo position

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -4,8 +4,8 @@ import { NavDesktop } from './nav-desktop'
 export const Topbar = () => {
   return (
     <div className="fixed top-0 left-0 right-0 lg:right-16 bg-neutral-950 lg:bg-transparent z-10">
-      <nav className="flex items-center justify-end py-1 lg:py-2">
-        <span className="lg:mr-48 lg:pr-24 pt-2 ">
+      <nav className="flex items-center justify-between py-1 lg:py-2">
+        <span className="pt-2 pl-4">
           <img
             src="/dc-photography-high-resolution-logo-transparent.webp"
             alt="logo"
